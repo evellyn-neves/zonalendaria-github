367a014d73bb57151ab0c279d0c6f73dc2ec00db
evellyn-neves <evellyn.maneves@gmail.com>
2024-09-07 23:42:32 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+function buscar() {
+    // Obtém a seção HTML onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    // se campoPesquisa for uma string sem nada
+    // if (!campoPesquisa) {
+    //     section.innerHTML = "<p>Nenhum resultado encontrado</p>"
+    //     return
+    // }
+
+    // Verifica se campoPesquisa está vazio após remover espaços em branco.
+    // Se estiver vazio, exibe uma mensagem de erro e interrompe a função.
+    if (!campoPesquisa.trim()) {
+        section.innerHTML = "<p>Por favor, digite um termo de busca válido.</p>";
+        return;
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    
+    // Inicializa uma string vazia para concatenar os resultados da pesquisa
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
+    // Itera sobre cada item da lista de dados (assumindo que 'dados' seja um array de objetos)
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        // se titulo includes campoPesquisa''
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)|| tags.includes(campoPesquisa)) {
+            // Cria uma nova div para cada resultado, formatando o HTML com os dados do objeto atual
+            resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="https://www.youtube.com/youtube.com/watch?v=ruS-PeRHLF0" target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href="${dado.link}" target="_blank">Saiba mais</a>
+            </div>
+        `;
+        }
+    }
+
+    if (!resultados) {
+        resultados = "<p>Nenhum resultado encontrado</p>"
+    }
+
+    // Atribui o HTML gerado à seção de resultados, substituindo o conteúdo anterior
+    section.innerHTML = resultados;
+    
+}
---
+++ dados.js
@@ -0,0 +1,63 @@
+let dados = [
+    {
+        titulo : "Super Mario Bros",
+        descricao : "Super Mario Bros é uma franquia de jogos eletrônicos criada pela Nintendo. O personagem principal, Mario, é um encanador italiano que viaja por diferentes mundos para salvar a Princesa Peach das garras de Bowser. A série é conhecida por sua jogabilidade simples e viciante, com Mario pulando sobre obstáculos e inimigos para alcançar a bandeira.",
+        link : "https://pt.wikipedia.org/wiki/Super_Mario",
+        plataforma: "NES",
+        genero: "Plataforma",
+        lancamento: 1985,
+        desenvolvedor: "Nintendo EAD",
+        tags: "plataforma nintendo mario clássico 2d"
+    },
+    {
+        titulo: "Sonic the Hedgehog",
+        descricao: "Sonic the Hedgehog é uma franquia de jogos eletrônicos criada pela Sega. O personagem principal, Sonic, é um ouriço azul rápido que luta contra o Dr. Eggman para salvar o mundo. A série é conhecida por sua jogabilidade rápida e divertida, com Sonic correndo por diversos cenários coloridos e desafiadores.",
+        link: "https://pt.wikipedia.com/wiki/Sonic_the_Hedgehog",
+        plataforma: "Mega Drive",
+        genero: "Plataforma",
+        lancamento: 1991,
+        desenvolvedor: "Sega/Sonic Team",
+        tags: "plataforma sega sonic velocidade ouriço dr. eggman"
+    },
+    {
+        titulo: "Fallout",
+        descricao: "Fallout foi lançado em 1997. Ele foi desenvolvido pela Black Isle Studios e publicado pela Interplay.O jogo se passa em um mundo pós-apocalíptico, resultado de uma grande guerra nuclear, onde os sobreviventes lutam para se adaptar a um novo mundo hostil. O jogo se destaca por sua rica história, diálogos complexos e um sistema de RPG profundo, permitindo que os jogadores personalizem seus personagens e façam escolhas que impactam o desenrolar da história.",
+        link: "https://pt.wikipedia.org/wiki/Fallout_(franquia)",
+        plataforma: "PC",
+        genero: "RPG eletrônico",
+        lancamento: 1997,
+        desenvolvedor: "Interplay Entertainment, Black Isle Studios, The Omni Group",
+        tags: "rpg pós-apocalíptico pos apocaliptico nuclear rpg eletrônico eletronico black isle studios interplay"
+    },
+    {
+        titulo: "The Legend of Zelda: A Link to the Past",
+        descricao: "The Legend of Zelda: A Link to the Past é um clássico jogo de ação e aventura desenvolvido pela Nintendo. O jogador controla Link, um jovem herói que embarca em uma jornada para salvar Hyrule de uma força maligna. O jogo é conhecido por seu mundo aberto, quebra-cabeças desafiadores e uma história rica.",
+        link: "https://pt.wikipedia.org/wiki/The_Legend_of_Zelda:_A_Link_to_the_Past",
+        plataforma: "Super Nintendo",
+        genero: "Plataforma",
+        lancamento: 1991,
+        desenvolvedor: "Nintendo",
+        tags: "plataforma aventura hyrule link nintendo zelda"
+    },
+    {
+        titulo: "Donkey Kong",
+        descricao: "Donkey Kong é um dos primeiros jogos de plataforma da história, criado por Shigeru Miyamoto para a Nintendo. O jogador controla Mario, que precisa escalar estruturas e evitar barris lançados por Donkey Kong para resgatar sua namorada, Pauline.",
+        link: "https://pt.wikipedia.org/wiki/Donkey_Kong",
+        plataforma: "Super Nintendo",
+        genero: "Plataforma",
+        lancamento: 1981,
+        desenvolvedor: "Nintendo",
+        tags: "plataforma nintendo donkey kong mario pauline clássico"
+    },
+    {
+        titulo: "Grand Theft Auto: San Andreas",
+        descricao: "Grand Theft Auto: San Andreas é um jogo de mundo aberto desenvolvido pela Rockstar Games. Ambientado em um estado fictício baseado na Califórnia, o jogador controla Carl Johnson, um ex-gangster que retorna para sua cidade natal e se envolve em uma série de crimes e missões.",
+        link: "https://pt.wikipedia.org/wiki/Grand_Theft_Auto_San_Andreas",
+        plataforma: "Consoles/PC",
+        genero: "Ação e aventura (mundo aberto)",
+        lancamento: 2004,
+        desenvolvedor: "Rockstar Games",
+        tags: "mundo aberto ação aventura rockstar games carl johnson gangue crime"
+    },
+];
+
